e68648dd03be26dc27b137d0573220d8d17bed59
stephenjude <stephenjudesuccess@gmail.com>
2022-04-08 09:33:21 +0100

    basic routing and view

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -5,15 +5,14 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Welcome</title>
+    <title>About</title>
 </head>
 <body>
     <h1 style="text-align: center">
-        Laravel 8 From Scratch Course
+        About Laravel 8 From Scratch Course
     </h1>
-
     <p style="text-align: center">
-        <a href="{{ route('about') }}">About</a>
+        <a href="{{ route('welcome') }}">Home</a>
     </p>
 </body>
 </html>
--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,5 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::view('/', 'welcome')->name('welcome');
+Route::view('/about', 'about')->name('about');
