642a2b275d1fb3f68e70ff0fa8e4d5aa76d4a9db
stephenjude <stephenjudesuccess@gmail.com>
2022-06-28 09:41:37 +0100

    eloquent orm

---
+++ database/migrations/2022_06_27_200438_create_products_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProductsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('slug')->unique()->nullable();
+            $table->longText('description')->nullable();
+            $table->string('image_url')->nullable();
+            $table->unsignedBigInteger('qty')->default(0);
+            $table->boolean('featured')->default(false);
+            $table->boolean('is_visible')->default(false);
+            $table->decimal('price', 10, 2)->nullable();
+            $table->date('published_at')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+}
