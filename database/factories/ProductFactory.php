58cf41e5a65f32eb5318a11fe6195d58a714cf0f
stephenjude <stephenjudesuccess@gmail.com>
2022-06-29 10:34:15 +0100

    model factories

---
+++ database/factories/ProductFactory.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class ProductFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            'name' => $name = $this->faker->unique()->catchPhrase(),
+            'slug' => Str::slug($name),
+            'description' => $this->faker->realText(),
+            'image_url' => $this->faker->imageUrl(),
+            'qty' => $this->faker->randomDigitNotNull(),
+            'featured' => $this->faker->boolean(),
+            'is_visible' => $this->faker->boolean(),
+            'price' => $this->faker->randomFloat(2, 80, 400),
+            'published_at' => $this->faker->dateTimeBetween('-1 year', '+1 year'),
+            'created_at' => $this->faker->dateTimeBetween('-1 year', '-6 month'),
+            'updated_at' => $this->faker->dateTimeBetween('-5 month', 'now'),
+        ];
+    }
+}
